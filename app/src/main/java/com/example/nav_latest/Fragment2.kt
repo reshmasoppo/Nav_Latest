0021b4fdd49fa9fd7e5b2c034781a7c82bb58f79
IN007848 <Reshma@oppo2019>
2019-12-09 18:10:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nav_latest/Fragment2.kt
@@ -0,0 +1,25 @@
+package com.example.nav_latest
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class Fragment2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment2, container, false)
+    }
+
+    }
+
+////https://medium.com/@stavro96/how-to-setup-android-navigation-component-with-drawer-88b833312661
